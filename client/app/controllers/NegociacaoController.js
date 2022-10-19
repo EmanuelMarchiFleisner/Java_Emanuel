2b8e3731b0fcbb920bab1c559187930e90462530
Emanuel Marchi Fleisner <emanuelxdx1@gmail.com>
2022-10-18 21:45:31 -0300

    segunda

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -10,18 +10,11 @@ class NegociacaoController{
 
     adciona(event) {
         event.preventDefault();
-        let data = new Date(...this._inputData.value.split('-').map(function (item, indice){
-            if (indice == 1) {
-                return item -1;
-            }
-            return item;
-        }));
+        let data = new Date(...this._inputData.value.split('-').map((item, indice) => item - (indice % 2)));
         console.log(data);
 
         let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
 
-        console.log(this._inputData.value);
-        console.log(this._inputQuantidade.value);
-        console.log(this._inputValor.value);
+        console.log(negociacao);
     }
 }
